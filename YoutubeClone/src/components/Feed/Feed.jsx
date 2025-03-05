40bc1d9740c5f5b6f7b125572f5234ecab4e870c
Aviral Dixit <aviraldxt345@gmail.com>
2025-03-05 20:10:45 +0530

    feedPageStarted

---
+++ YoutubeClone/src/components/Feed/Feed.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import './Feed.css';
+
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+
+const Feed = () => {
+  return (
+    <div className="feed">
+
+      <div className='card'>
+         <img src={thumbnail1} alt="thumb1" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail2} alt="thumb2" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail3} alt="thumb3" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail4} alt="thumb4" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail5} alt="thumb5" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail6} alt="thumb6" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail7} alt="thumb7" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+      <div className='card'>
+         <img src={thumbnail8} alt="thumb8" />
+         <h2>Best location to visit in your holidays with your family and friends</h2>
+         <h3>TravellerHub</h3>
+         <p>15k views &bull; 3 days ago</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Feed
