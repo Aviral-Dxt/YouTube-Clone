257a7a8ba8d16ae5d035dabb5b6187ad3b8c871f
Aviral Dixit <aviraldxt345@gmail.com>
2025-03-29 18:05:28 +0530

    API-IntegrationStarted

---
+++ YoutubeClone/src/Data.js
@@ -0,0 +1,3 @@
+export const API_KEY= 'AIzaSyDuZVRirvu44rmjCxByJkpTRtwoKQ-pN3Q';
+
+
--- YoutubeClone/src/components/Feed/Feed.jsx
+++ YoutubeClone/src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css';
 
 import thumbnail1 from '../../assets/thumbnail1.png'
@@ -10,156 +10,62 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom';
+import { API_KEY } from '../../Data';
 
-const Feed = () => {
 
-  return (
-    <div className="feed">
 
-      <Link to ={`video/20/4521`} className='card'>
-         <img src={thumbnail1} alt="thumb1" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </Link >
 
-      <div className='card'>
-         <img src={thumbnail2} alt="thumb2" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
+const Feed = ({ category }) => {
+
+   const [deta, setDeta] = useState([]);
+
+   const fetchDeta = async () => {
+
+      let videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+
+      try {
+
+         const response = await fetch(videoList_url);
+         const deta = await response.json();
+
+         setDeta(deta.items);
+
+      } catch (error) {
+         console.log(error);
+
+      }
+   }
+
+   useEffect(() => {
+      fetchDeta()
+
+   }, [category])
+
+
+
+   return (
+      <div className="feed">
+
+       {deta.map((item,index)=>{
+         return (
+             
+            <Link to={`video/20/4521`} className='card'>
+            <img src={thumbnail1} alt="thumb1" />
+            <h2>Best location to visit in your holidays with your family and friends</h2>
+            <h3>TravellerHub</h3>
+            <p>15k views &bull; 3 days ago</p>
+            
+         </Link >
+
+         )
+       })}
+
+        
+
+
+
       </div>
-
-      <div className='card'>
-         <img src={thumbnail3} alt="thumb3" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail4} alt="thumb4" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail5} alt="thumb5" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail6} alt="thumb6" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail7} alt="thumb7" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail8} alt="thumb8" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail1} alt="thumb1" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail2} alt="thumb2" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail3} alt="thumb3" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail4} alt="thumb4" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail5} alt="thumb5" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail6} alt="thumb6" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail7} alt="thumb7" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail8} alt="thumb8" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail1} alt="thumb1" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail2} alt="thumb2" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail3} alt="thumb3" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-
-      <div className='card'>
-         <img src={thumbnail4} alt="thumb4" />
-         <h2>Best location to visit in your holidays with your family and friends</h2>
-         <h3>TravellerHub</h3>
-         <p>15k views &bull; 3 days ago</p>
-      </div>
-       
-
-
-    </div>
-  )
+   )
 }
 
 export default Feed
