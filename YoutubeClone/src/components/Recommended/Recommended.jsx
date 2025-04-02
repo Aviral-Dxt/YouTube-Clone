5c1bac580950abdf0b1e1c77899aad0f378eb166
Aviral Dixit <aviraldxt345@gmail.com>
2025-04-02 16:22:18 +0530

    RecommendedSectionCreated

--- YoutubeClone/src/components/Recommended/Recommended.jsx
+++ YoutubeClone/src/components/Recommended/Recommended.jsx
@@ -1,91 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Recommended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
+import { API_KEY, valueConverter } from '../../Data'
 
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+
+    const [apiDeta, setApiDeta] = useState([]);
+
+    const fetchDeta = async () => {
+
+        const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+
+        await fetch(url).then((res) => res.json()).then(deta => setApiDeta(deta.items));
+
+    }
+
+
+
+    useEffect(() => {
+        fetchDeta()
+    }, [])
+
+
     return (
         <div className='recommended'>
 
-            
-            <div className="side-video-list">
-                <img src={thumbnail1} alt="thumbnail1" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+            {console.log(apiDeta)}
 
-            <div className="side-video-list">
-                <img src={thumbnail2} alt="thumbnail2" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+            {apiDeta.map((item, index) => {
 
-            <div className="side-video-list">
-                <img src={thumbnail3} alt="thumbnail3" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+                return (
 
-            <div className="side-video-list">
-                <img src={thumbnail4} alt="thumbnail4" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+                    <div key={index} className="side-video-list">
+                        <img src={item.snippet.thumbnails.medium.url} alt="thumbnail1" />
+                        <div className="vid-info">
+                            <h4>{item.snippet.title}</h4>
+                            <p>{item.snippet.channelTitle}</p>
+                            <p>{valueConverter(item.statistics.viewCount)} views</p>
+                        </div>
+                    </div>
 
-            <div className="side-video-list">
-                <img src={thumbnail5} alt="thumbnail5" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+                )
 
-            <div className="side-video-list">
-                <img src={thumbnail6} alt="thumbnai6" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
+            })}
 
-            <div className="side-video-list">
-                <img src={thumbnail7} alt="thumbnail7" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
-
-            <div className="side-video-list">
-                <img src={thumbnail8} alt="thumbnai8" />
-                <div className="vid-info">
-                    <h4>Best Channel that help you to become a good person</h4>
-                    <p>FitTuber</p>
-                    <p>199k views</p>
-                </div>
-            </div>
 
         </div>
     )
